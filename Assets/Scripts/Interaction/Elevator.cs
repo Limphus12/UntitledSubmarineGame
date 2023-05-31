42450ec5fcd919715673f2c29d8a1f239d598d39
Limphus12 <Limphus12@gmail.com>
2023-05-31 17:49:22 +0100

    31/05/2023 - Update 002
    added elevator code and an elevator prefab!

---
+++ Assets/Scripts/Interaction/Elevator.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] float positionSmooth;
+    bool isMoving;
+
+    Vector3 targetPos;
+
+    private void Awake()
+    {
+        isMoving = false;
+        targetPos = transform.position;
+    }
+
+    public void SetTargetPos(float y)
+    {
+        targetPos = new Vector3(transform.position.x, y, transform.position.z);
+    }
+
+    private void Update()
+    {
+        if (isMoving) Move(targetPos);
+    }
+
+    public void StartMove()
+    {
+        moveI = 0;
+        isMoving = true;
+    }
+
+    float moveI = 0;
+    void Move(Vector3 position) 
+    {
+        moveI += Time.deltaTime * positionSmooth;
+
+        //apply target rotation
+        transform.localPosition = Vector3.Lerp(transform.localPosition, position, moveI);
+
+        if (moveI >= 1)
+        {
+            EndMove();
+        }
+    }
+
+    void EndMove()
+    {
+        moveI = 0;
+        isMoving = false;
+    }
+}
