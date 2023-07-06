a35f93051f57b681a9cfc33f79cc6c08a4a1c321
Limphus12 <Limphus12@gmail.com>
2023-07-06 19:59:14 +0100

    hit anim curves

--- Assets/Scripts/Mining/Gem.cs
+++ Assets/Scripts/Mining/Gem.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Gem : MonoBehaviour, IMineable
 {
+    [Header("Attributes")]
     [SerializeField] private int mineAmount;
     [SerializeField] private float miningCooldown;
 
     [Space, SerializeField] private Vector2 pointMinMax;
 
+    [Header("VFX")]
+    [SerializeField] private GameObject gfx;
+
+    [SerializeField] private GameObject hitParticles;
     [Space, SerializeField] private AnimationCurve[] miningHitCurves;
+
+    [Header("SFX")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] hitClips;
 
     private bool canMine = true;
 
@@ -20,16 +29,18 @@ public class Gem : MonoBehaviour, IMineable
         currentMineAmount = 0;
     }
 
+    private void Update()
+    {
+        if (!canMine) SizeLerp();
+        else EndSizeLerp();
+    }
+
     public bool CanMine() => canMine;
 
     public void Mine()
     {
         MiningEffects();
 
-        currentMineAmount++;
-
-        //do particle effects
-        //do sound effects
         //add points (randomize amount)
 
         if (currentMineAmount >= mineAmount) EndMining();
@@ -39,8 +50,34 @@ public class Gem : MonoBehaviour, IMineable
 
     private void MiningEffects()
     {
+        if (hitParticles)
+        {
+            GameObject particle = Instantiate(hitParticles, transform.position, Quaternion.identity);
+            Destroy(particle, 1f);
+        }
 
+        if (audioSource && hitClips.Length > 0)
+        {
+            audioSource.PlayOneShot(hitClips[Random.Range(0, hitClips.Length)]);
+        }
     }
+
+    float sizeI = 0;
+
+    private void SizeLerp()
+    {
+        sizeI += Time.deltaTime;
+
+        float i = miningHitCurves[currentMineAmount].Evaluate(sizeI);
+
+        gfx.transform.localScale = new Vector3(i, i, i);
+    }
+
+    private void EndSizeLerp()
+    {
+        sizeI = 0;
+    }
+
 
     private void EndMining()
     {
@@ -52,6 +89,8 @@ public class Gem : MonoBehaviour, IMineable
         canMine = false;
 
         yield return new WaitForSeconds(miningCooldown);
+
+        currentMineAmount++;
 
         canMine = true;
     }
