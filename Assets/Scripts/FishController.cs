947271a3f5707455ea7c810e952d7f7414371b23
Limphus12 <Limphus12@gmail.com>
2023-08-02 21:09:05 +0100

    wandering!

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FishMovementType { WANDER, CHASE }
+
 public class FishController : MonoBehaviour
 {
     [Header("Movement")]
@@ -11,7 +13,6 @@ public class FishController : MonoBehaviour
 
     [Header("Rotation")]
     [SerializeField] private float rotateSpeed;
-
 
     [Header("Prediction")]
     [SerializeField] private float maxDistancePredict = 100;
@@ -23,6 +24,14 @@ public class FishController : MonoBehaviour
     [SerializeField] private float deviationAmount = 50;
     [SerializeField] private float deviationSpeed = 2;
 
+    [Header("Wandering")]
+    [SerializeField] private float wanderTargetRadius;
+
+    public GameObject testObject;
+
+    [SerializeField] private FishMovementType movementType;
+
+    private Vector3 wanderTargetPos;
 
     [Space, SerializeField] private Transform target;
 
@@ -33,25 +42,97 @@ public class FishController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         if (target) targetRb = target.GetComponent<Rigidbody>();
+
+        if (movementType == FishMovementType.WANDER)
+        {
+            CalculateWanderTarget();
+        }
     }
 
-    float CalculateLeadTimePercentage() => Mathf.InverseLerp(minDistancePredict, maxDistancePredict, Vector3.Distance(transform.position, target.transform.position));
+    protected void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    float CalculateLeadTimePercentage(Vector3 targetPos) => Mathf.InverseLerp(minDistancePredict, maxDistancePredict, Vector3.Distance(transform.position, targetPos));
 
     private void FixedUpdate()
     {
-        var leadTimePercentage = CalculateLeadTimePercentage();
+        CheckState(); Rotate(); Move();
+    }
 
-        PredictMovement(leadTimePercentage);
+    private void CheckState()
+    {
+        float leadTimePercentage;
 
-        AddDeviation(leadTimePercentage);
+        switch (movementType)
+        {
+            case FishMovementType.WANDER:
 
-        Rotate();
+                Wander();
 
-        Move();
+                leadTimePercentage = CalculateLeadTimePercentage(wanderTargetPos);
+
+                AddDeviation(wanderTargetPos);
+
+                break;
+
+            case FishMovementType.CHASE:
+
+                leadTimePercentage = CalculateLeadTimePercentage(target.transform.position);
+
+                PredictMovement(leadTimePercentage);
+
+                AddDeviation(leadTimePercentage);
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Wander()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        float distance = Vector3.Distance(wanderTargetPos, transform.position);
+
+        Debug.Log(distance);
+
+        if (distance < 10f)
+        {
+            CalculateWanderTarget();
+        }
+    }
+
+    private void CalculateWanderTarget()
+    {
+        Vector3 newTargetPos = UnityEngine.Random.insideUnitSphere * wanderTargetRadius + transform.position;
+
+        float distance = Vector3.Distance(transform.position, newTargetPos);
+
+        if (Physics.Raycast(transform.position, newTargetPos - transform.position, distance))
+        {
+            CalculateWanderTarget(); return;
+        }
+
+        else if (newTargetPos.y > -25f)
+        {
+            CalculateWanderTarget(); return;
+        }
+
+        wanderTargetPos = newTargetPos;
+        if (testObject) testObject.transform.position = wanderTargetPos;
     }
 
     private void PredictMovement(float leadTimePercentage)
     {
+        if (!targetRb) return;
+
         var predictionTime = Mathf.Lerp(0, maxTimePrediction, leadTimePercentage);
 
         standardPrediction = targetRb.position + targetRb.velocity * predictionTime;
@@ -66,12 +147,38 @@ public class FishController : MonoBehaviour
         deviatedPrediction = standardPrediction + predictionOffset;
     }
 
+    private void AddDeviation(Vector3 target)
+    {
+        var deviation = new Vector3(Mathf.Cos(Time.time * deviationSpeed), 0, 0);
+
+        var predictionOffset = transform.TransformDirection(deviation) * deviationAmount;
+
+        deviatedPrediction = target + predictionOffset;
+    }
+
     private void Rotate()
     {
-        var heading = deviatedPrediction - transform.position;
+        switch (movementType)
+        {
+            case FishMovementType.WANDER:
 
-        var rotation = Quaternion.LookRotation(heading);
-        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed * Time.fixedDeltaTime));
+                var wanderHeading = wanderTargetPos - transform.position;
+
+                var wanderRotation = Quaternion.LookRotation(wanderHeading);
+                rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, wanderRotation, rotateSpeed * Time.fixedDeltaTime));
+
+                break;
+            case FishMovementType.CHASE:
+
+                var chaseHeading = deviatedPrediction - transform.position;
+
+                var chaseRotation = Quaternion.LookRotation(chaseHeading);
+                rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, chaseRotation, rotateSpeed * Time.fixedDeltaTime));
+
+                break;
+            default:
+                break;
+        }
     }
 
     private void Move()
