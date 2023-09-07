5e88a76f50240ee39fb90d772fc32a592d295a17
Limphus12 <Limphus12@gmail.com>
2023-09-07 11:49:51 +0100

    main menu fade in/out

--- Assets/Scripts/Interaction/SceneLoader.cs
+++ Assets/Scripts/Interaction/SceneLoader.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    [SerializeField] private Animator fadeToBlackAnimator;
+    [SerializeField] private float fadeTime;
+
+    public void LoadScene(int scene)
     {
-        SceneManager.LoadScene(sceneName);
+        fadeToBlackAnimator.SetTrigger("fadeTo");
+
+        StartCoroutine(LoadSceneFade(scene));
+    }
+
+    public void LoadScene(string scene)
+    {
+        fadeToBlackAnimator.SetTrigger("fadeTo");
+
+        StartCoroutine(LoadSceneFade(scene));
+    }
+
+    private IEnumerator LoadSceneFade(int scene)
+    {
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadSceneAsync(scene);
+    }
+
+    private IEnumerator LoadSceneFade(string scene)
+    {
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadSceneAsync(scene);
     }
 }
