f237ac81de82d3fa4456dee9a9d3c934788bab46
Limphus12 <Limphus12@gmail.com>
2023-09-07 13:18:34 +0100

    working on topside ui

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,47 @@ public class GameStateManager : MonoBehaviour
 {
     public enum GameState { PLAY, PAUSE }
 
-    private GameState currentState;
+    private static GameState currentState = GameState.PLAY;
+
+    public event EventHandler<OnGameStateChangedEventArgs> OnGameStateChanged;
+
+    public class OnGameStateChangedEventArgs : EventArgs { public GameState i; }
 
     public void SwitchState(GameState newState)
     {
         if (newState == currentState) return;
 
         currentState = newState;
+
+        //firing off our event here
+        OnGameStateChanged?.Invoke(this, new OnGameStateChangedEventArgs { i = currentState });
     }
 
     public GameState GetGameState() => currentState;
+
+    private void Update()
+    {
+        Inputs();
+    }
+
+    private void Inputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            switch (currentState)
+            {
+                case GameState.PLAY:
+
+                    SwitchState(GameState.PAUSE);
+
+                    break;
+
+                case GameState.PAUSE:
+
+                    SwitchState(GameState.PLAY);
+
+                    break;
+            }
+        }
+    }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,52 +5,38 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("UI")]
+    private GameStateManager gameStateManager;
 
+    [SerializeField] private GameObject quitButton;
 
-    public TMP_Text goldUI;
-
-    public GameObject jailUI, jailUIText;
-
-    public Animator jailAnim;
-
-    public float jailUIMidTime, jailUIEndTime;
-
-    public string jailAnimationName = "Fade";
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //make sure we have our game state manager
+        if (!gameStateManager) gameStateManager = FindObjectOfType<GameStateManager>();
+
+        //and subscribe to the events!
+        gameStateManager.OnGameStateChanged += CheckGameState;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckGameState(object sender, GameStateManager.OnGameStateChangedEventArgs e)
     {
+        if (!quitButton) return;
+
+        switch (e.i)
+        {
+            case GameStateManager.GameState.PLAY:
+
+                quitButton.SetActive(false);
+
+                break;
+            case GameStateManager.GameState.PAUSE:
+
+                quitButton.SetActive(true);
+
+                break;
+
+            default:
+                break;
+        }
     }
-
-    public void JailUIStart()
-    {
-        jailUI.SetActive(true);
-        jailUIText.SetActive(true);
-
-        jailAnim.Play(jailAnimationName);
-
-        Invoke("JailUIMid", jailUIMidTime);
-        Invoke("JailUIEnd", jailUIEndTime);
-    }
-
-    public void JailUIMid()
-    {
-        jailUIText.SetActive(false);
-    }
-
-    public void JailUIEnd()
-    {
-        jailUI.SetActive(false);
-    }
-
 }
